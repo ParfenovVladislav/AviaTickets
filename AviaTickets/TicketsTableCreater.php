3520af258f1a93bbb9eee564de7eae96e4f61137
PepperDe <30377709+PepperDe@users.noreply.github.com>
2019-05-27 19:11:19 +0300

    Add files via upload

---
+++ AviaTickets/TicketsTableCreater.php
@@ -0,0 +1,21 @@
+<?php
+    header( 'Content-Type: text/html; charset=utf-8');
+
+    $config = array (
+        "token" => "3ddd617e-7361-4ab4-b5c5-0fde95d179d0",
+    );
+
+    $url = "https://api.rasp.yandex.net/v3.0/search/?apikey=3ddd617e-7361-4ab4-b5c5-0fde95d179d0&format=json&from=cLED&to=c213&lang=ru_RU&page=1&date=2019-05-27&transport_types=plane";
+    $content = file_get_contents($url);
+    $jsonResult = json_decode($content);          
+    if (1)
+        {
+            echo "&nbsp Total flights ".count($jsonResult->segments).""; 
+            echo "<br>From ".$jsonResult->segments[0]->from->title." to ".$jsonResult->segments[0]->to->title."";
+            echo "<table><tr><th>Departure time</th><th>Arrival time</th><th>Время в пути</th><Билеты от></tr>";
+                foreach($jsonResult->segments as $flight){  
+                    echo "<tr><td>".substr($flight->departure,11,8)."</td><td>".substr($flight->arrival,11,8)."</td>";
+                    echo "</tr>";
+            }
+    }
+?>
--- AviaTickets/index.php
+++ AviaTickets/index.php
@@ -5,77 +5,102 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/includes/header.php');
 
 <div id="main">
 
-<div id="primary">
-  <div id="content" role="main">
-    <div class="form-container">
-      <div class="response"></div>
-      <form class="forms" action="#" method="post">
-        <fieldset>
-<table align="center" width="100%" border="1">
-	<tr>
-		<td>
-		<section>
-    <label class="select-label">Leaving from:</label>
-<input type="text" name="leaving" class="text-input defaultText required email" title="Moscow, Russia"/>
-</section>
-		</td>
-		<td>
-		<section>
-    <label class="select-label">Going to:</label>
-<input type="text" name="going" class="text-input defaultText required email" title="Liverpool, United Kingdom"/>
-</section>
-</td>
-	</tr>
-	<tr>
-		<td>
-		<section>
-    <label class="select-label">Departuring:</label>
-<input type="text" name="email" class="text-input defaultText required email" title="Date"/>
-</section>
-		</td>
-		<td>
-		<section>
-    <label class="select-label">Preferred class:</label>
-	
-		<div class="text-input defaultText required email">
-    <select class="new-select-style-wpandyou>
-        <option value="1">No preference</option>
-        <option value="2">First class</option>
-        <option value="3">Business</option>
-		<option value="3">Economy</option>
-    </select>
-	</div>
-</section>
-		</td>
-	</tr>
-</table>
+    <div id="primary">
+        <div id="content" role="main">
+            <div class="form-container">
+                <div class="response"></div>
+                <div class="forms">
+                    <fieldset>
+                        <table align="center" width="100%" border="1">
+                            <tr>
+                                <td>
+                                    <section>
+                                        <label class="select-label">Leaving from:</label>
+                                        <input type="text" name="leaving" class="text-input defaultText required email"
+                                            title="City or airport" />
+                                    </section>
+                                </td>
+                                <td>
+                                    <section>
+                                        <label class="select-label">Going to:</label>
+                                        <input type="text" name="going" class="text-input defaultText required email"
+                                            title="City or airport" />
+                                    </section>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <section>
+                                        <label class="select-label">Departuring:</label>
+                                        <input type="text" id="inputDate" class="text-input defaultText required email"
+                                            title="Date" />
+                                    </section>
+                                </td>
+                                <td>
+                                    <section>
+                                        <label class="select-label">Preferred class:</label>
 
-              <input type="submit" value="Search" name="Search" class="btn-submit" />
-        </fieldset>
-      </form>
-    </div>
- 
-     <div class="map full">
-    	<iframe width="1040" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?ll=40.967132,29.07403&amp;spn=0.047894,0.132093&amp;vpsrc=6&amp;t=m&amp;z=14&amp;output=embed"></iframe>
-    </div>
-	   
-	
-  
-  </div><!-- #content -->
-</div><!-- #primary -->
+                                        <div class="text-input defaultText required email">
+                                            <select class="new-select-style-wpandyou>
+        										<option value=" 1">No preference</option>
+                                                <option value="2">First class</option>
+                                                <option value="3">Business</option>
+                                                <option value="3">Economy</option>
+                                            </select>
+                                        </div>
+                                    </section>
+                                </td>
+                            </tr>
+                        </table>
+
+                        <input type="submit" id="Search" class="btn-submit" />
+                    </fieldset>
+                </div>
+
+
+            </div>
+
+            <div id="ticketsDiv">
+            </div>
+
+            <div class="map full">
+                <iframe width="1040" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
+                    src="http://maps.google.com/maps?ll=40.967132,29.07403&amp;spn=0.047894,0.132093&amp;vpsrc=6&amp;t=m&amp;z=14&amp;output=embed"></iframe>
+            </div>
+
+
+
+        </div><!-- #content -->
+    </div><!-- #primary -->
 
 
 </div><!-- #main -->
 
-	<footer id="colophon" role="contentinfo">
-		<div id="site-generator">
-			Copyright 2019 - Parfenov & Nekrut
-		</div>
+<footer id="colophon" role="contentinfo">
+    <div id="site-generator">
+        Copyright 2019 - Parfenov & Nekrut
+    </div>
 
-	</footer><!-- #colophon -->
-	</div><!-- #wrapper -->
+</footer><!-- #colophon -->
+</div><!-- #wrapper -->
 </div><!-- #page -->
 
 <script type="text/javascript" src="style/js/scripts.js"></script>
 </body>
+
+<script>
+$('#Search').click(function() {
+    var area = $("#ticketsDiv");
+    area.load("TicketsTableCreater.php");
+});
+
+
+$(document).ready(function() {
+
+    // assuming the controls you want to attach the plugin to
+    // have the "datepicker" class set
+    $('#inputDate').Zebra_DatePicker();
+});
+</script>
+
 </html>
