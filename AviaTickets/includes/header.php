e4e0ae43868b76854c78a876adcbeec06f817d7d
PepperDe <30377709+PepperDe@users.noreply.github.com>
2019-05-30 17:52:47 +0300

    Add files via upload

--- AviaTickets/includes/header.php
+++ AviaTickets/includes/header.php
@@ -16,11 +16,14 @@ if (isset ($_GET['exit']))
     <link rel="shortcut icon" href="style/images/favicon.png" />
     <link rel="stylesheet" type="text/css" href="style/css/style.css" media="all" />
     <link rel="stylesheet" type="text/css" href="style/css/zebra_datepicker.min.css" media="all" />
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">  <!-- ДОБАВЛЕНО -->
+    <link rel="stylesheet" type="text/css" href="style/type/marketdeco.css" media="all"/>
 
     <!-- <script type="text/javascript" src="style/js/jquery-1.7.1.min.js"></script> -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     <script type="text/javascript" src="style/js/ddsmoothmenu.js"></script>
     <script src="style\js\zebra_datepicker.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>   <!-- ДОБАВЛЕНО -->
 </head>
 
 <body>
